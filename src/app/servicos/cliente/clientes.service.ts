215cd378358409de89a7baa43a05d643da962f6e
felipeoliveir44 <felipeoliveiragg@outlook.com>
2024-03-24 17:47:12 -0300

    Feat: backend da pagina de compras

--- src/app/servicos/cliente/clientes.service.ts
+++ src/app/servicos/cliente/clientes.service.ts
@@ -5,42 +5,28 @@ import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Cliente } from 'src/app/models/cliente';
 import { Page } from 'src/app/models/page';
-
-  
-  
-
 @Injectable({
-
-  providedIn: 'root'
-
+  providedIn: 'root',
 })
-
 export class ClientesService {
+  private url: string = 'http://localhost:8080/cliente/listar';
 
-  
+  private post: string = 'http://localhost:8080/cliente/cadastrar';
 
-  private url:string = 'http://localhost:8080/cliente/listar'
+  private delete: string = 'http://localhost:8080/cliente/desativar/';
 
-  private post:string = 'http://localhost:8080/cliente/cadastrar'
+  constructor(private http: HttpClient) {}
 
-  private delete:string = 'http://localhost:8080/cliente/desativar/'
-
-  
-
-  constructor(private http: HttpClient) { }
-
-  
-
-  listar(pagina: number): Observable<Page<Cliente>> {
+  listar(pagina: number): Observable<Page<Cliente>> {
     return this.http.get<Page<Cliente>>(`${this.url}?page=${pagina}`);
   }
 
- cadastrar(obj: any):Observable<any[]>{
-    return this.http.post<any[]>(this.post, obj)
-  }
+  cadastrar(obj: any): Observable<any[]> {
+    return this.http.post<any[]>(this.post, obj);
+  }
 
-  deletarCliente(clienteId: number): Observable<any> {
-    const url = `${this.delete}${clienteId}`;
-    return this.http.delete<any>(url);
-  }
+  deletarCliente(clienteId: number): Observable<any> {
+    const url = `${this.delete}${clienteId}`;
+    return this.http.delete<any>(url);
+  }
 }
